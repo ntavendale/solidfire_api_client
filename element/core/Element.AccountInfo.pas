d3d2136ef20fd1225a2d13696ae94a34fe120540
Nigel Tavendale <nigel.tavendale@netapp.com>
2019-11-15 15:35:26 -0700

    Initial

---
+++ element/core/Element.AccountInfo.pas
@@ -0,0 +1,61 @@
+unit Solidfire.AccountInfo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  {$IFDEF FPC}
+  Classes, SysUtils, fpjson, jsonparser,
+  {$ELSE}
+  System.Classes, System.SysUtils, System.Json,
+  {$ENDIF}
+  SolidFire.Interfaces;
+
+type
+  TAccountInfo = class(TInterfacedObject, ISolidFire)
+  protected
+    FAccountID: Integer;
+    FInitiatorSecret: String;
+    FTargetSecret: String;
+  public
+    function Serialize: String;
+    procedure Deserialize(AValue: String);
+    class function Deserialize(AValue: String): TAccountInfo;
+    property AccountID: Integer read FAccountID write FAccountID;
+    property InitiatorSecret: String read FInitiatorSecret write FInitiatorSecret;
+    property TargetSecret: String read FTargetSecret write FTargetSecret;
+  end;
+
+implementation
+
+function TAccountInfo.Serialize: String;
+var
+  LJsonObject: TJsonObject;
+begin
+  LJsonObject := TJsonObject.Create;
+  try
+    {$IFDEF FPC}
+    LJsonObject.Add('accountId', FAccountID);
+    {$ELSE}
+    LJsonObject.AddPair('accountId', TJSonNumber.Create(FAccountID));
+    {$ENDIF}
+    Result := LJsonObject.AsJson;
+  finally
+    LJsonObject.Free;
+  end;
+end;
+
+procedure TAccountInfo.Deserialize(AValue: String);
+begin
+  //Do nothing!
+end;
+
+class function TAccountInfo.Deserialize(AValue: String): TAccountInfo;
+begin
+  Result := TAccountInfo.Create;
+  Result.Deserialize(AValue);
+end;
+
+end.
+
---
+++ element/core/solidfire.interfaces.pas
@@ -0,0 +1,20 @@
+unit SolidFire.Interfaces;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  ISolidFire = interface
+    ['{8600BDEE-9BDD-40BD-A388-C40A2D36A959}']
+    function Serialize: String;
+    procedure Deserialize(AValue: String);
+  end;
+
+implementation
+
+end.
+
